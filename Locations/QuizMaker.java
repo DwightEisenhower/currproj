6119e115f55c983e0a48e2cef0264e1e325e759e
16085 <16085@hs-90535-103.lan.summit.k12.nj.us>
2018-01-09 09:45:23 -0500

    must finish today

--- Locations/QuizMaker.java
+++ Locations/QuizMaker.java
@@ -1,59 +1,76 @@
 import java.util.*;
 import java.io.*;
-/**Error Codes (would go in the readme but 1. I don't submit it 2. no one reads it)
+/**System Exit Codes (would go in the readme but 1.I don't submit it; 2.no one reads it)
  * 1 = IO exception
  */
 public class QuizMaker {
     private static BufferedReader reader;
     private static FileWriter writer;
-    private static Random rand = new Random();
     private static String[] ffl = new String[]{"A","B","C","D"};
     public static void main() {
-        File output = new File("/SMB/16085/hs2.lan.summit.k12.nj.us/students/16085/Documents/Programming/currproj/Locations/quizzes/locations.txt");
         try {
-            if(!output.exists()) {
-                output.mkdir();
+            File directory = new File("/SMB/16085/hs2.lan.summit.k12.nj.us/students/16085/Documents/Programming/currproj/Locations/quizzes/");
+            File output = new File("/SMB/16085/hs2.lan.summit.k12.nj.us/students/16085/Documents/Programming/currproj/Locations/quizzes/locations.txt");
+            if(!directory.exists()) {
+                directory.mkdir();
                 output.createNewFile();
             }
             File input = new File("/SMB/16085/hs2.lan.summit.k12.nj.us/students/16085/Documents/Programming/currproj/Locations/states.txt");
             if(input.canRead() && output.canWrite())
                 makeQuiz(input, output);
+        } catch(FileNotFoundException ioe) {
+            System.out.println("File not found");
+            ioe.printStackTrace();
+            System.exit(1);
         } catch(IOException ioe) {
-            System.out.println("Unforseen IO exception...");
+            System.out.println("Unforseen IO exception in main()");
             ioe.printStackTrace();
             System.exit(1);
         }
     }
     
     public static void makeQuiz(File in, File out) {
-        HashMap<String, String> map = loadStateHash(in);
-        List<String> keys = new ArrayList<>(map.keySet());
-        rand = new Random();
+        HashMap<Key, String> map = loadStateHash(in);
+        List<Key> keys = new ArrayList<>(map.keySet());
         ArrayList<String> quizQuestions = new ArrayList<>();
         for(int i = 0; i < 50; i++) {
             String question = (i+1)+". The capital of ";
-            String state = keys.remove(rand.nextInt(keys.size()));//Choose random state & remove it
-            String[] answers = new String[4];
-            answers[rand.nextInt(4)] = map.get(state);//set the real answer to a random one from 1 to 4
-            question += state+" is...\n";
-            for(int j = 0; j < 4; j++)
-                if(answers[j] == null && !keys.isEmpty())
-                    answers[j] = map.get(keys.get(random(0,keys.size())));
-            for(int j = 0; j < 4; j++)
-                question += answers[j]+"\n";
+            String state = "";
+            //Choose random state & remove it
+            boolean valid = false;
+            while(!valid) {
+                int rand = random(0,keys.size());
+                if(!keys.get(rand).used) {
+                    state = keys.get(rand).str;
+                    valid = true;
+                    question += state+" is...\n";
+                }
+            }
+            ArrayList<String> answers = new ArrayList<>(4);
+            System.out.println(answers);
+            answers.add(map.get(new Key(state)));//set the real answer to a random one from 1 to 4
+            System.out.println(answers);
+            while(answers.size() < 4)
+                if(!keys.get(random(0,keys.size())).str.equals(state))
+                    answers.add(map.get(keys.get(random(0,keys.size()))));
+            System.out.println(answers);
+            //Collections.shuffle(answers);
+            while(!answers.isEmpty())
+                question += answers.remove(0)+"\n";
+            System.out.println(answers);
             quizQuestions.add(question);
         }
         writeToFile(out, quizQuestions);
     }
     
-    private static HashMap<String, String> loadStateHash(File in) {
+    private static HashMap loadStateHash(File in) {
         ArrayList<String> list = readFromFile(in);//odd index = state name, even index = capital
         if(list.isEmpty()) {
             System.out.println("states.txt is either empty or nonexistent");
         }
-        HashMap<String, String> map = new HashMap<String, String>();
+        HashMap<Key, String> map = new HashMap<Key, String>();
         while(!list.isEmpty())
-            map.put(list.remove(0), list.remove(0));
+            map.put(new Key(list.remove(0)), list.remove(0));
         return map;
     }
     
@@ -71,7 +88,7 @@ public class QuizMaker {
             
             return lines;
         } catch(IOException ioe) {
-            System.out.println("Unforseen IO exception...");
+            System.out.println("Unforseen IO exception while reading from file");
             ioe.printStackTrace();
             System.exit(1);
             return null;
@@ -89,7 +106,7 @@ public class QuizMaker {
             writer.close();
             return true;
         } catch(IOException ioe) {
-            System.out.println("Unforseen IO exception...");
+            System.out.println("Unforseen IO exception while writing to file");
             ioe.printStackTrace();
             System.exit(1);
             return false;
@@ -102,5 +119,13 @@ public class QuizMaker {
     
     private static void print(Object o) {
         System.out.println(o);
+    }
+    public static class Key {
+        public String str;
+        public boolean used;
+        public Key(String in) {
+            str = in;
+            used = false;
+        }
     }
 }
