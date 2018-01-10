d3509ab367798a80c113cb71e0c4525b093da8da
ErichvonManstein <lich.king.terminator@gmail.com>
2018-01-09 22:19:44 -0500

    locations is fucked

--- Locations/QuizMaker.java
+++ Locations/QuizMaker.java
@@ -2,6 +2,8 @@ import java.util.*;
 import java.io.*;
 /**System Exit Codes (would go in the readme but 1.I don't submit it; 2.no one reads it)
  * 1 = IO exception
+ * 
+ * NOTE: THIS ONLY WORKS WITH MAC OS BECAUSE OF THE POOR FILE KEEPING SYSTEM
  */
 public class QuizMaker {
     private static BufferedReader reader;
@@ -10,14 +12,15 @@ public class QuizMaker {
     public static void main() {
         try {
             File directory = new File("/SMB/16085/hs2.lan.summit.k12.nj.us/students/16085/Documents/Programming/currproj/Locations/quizzes/");
-            File output = new File("/SMB/16085/hs2.lan.summit.k12.nj.us/students/16085/Documents/Programming/currproj/Locations/quizzes/locations.txt");
-            if(!directory.exists()) {
+            File output = new File("/SMB/16085/hs2.lan.summit.k12.nj.us/students/16085/Documents/Programming/currproj/Locations/quizzes/locations0.txt");
+            if(!directory.exists() && !output.exists()) {
                 directory.mkdir();
                 output.createNewFile();
             }
             File input = new File("/SMB/16085/hs2.lan.summit.k12.nj.us/students/16085/Documents/Programming/currproj/Locations/states.txt");
             if(input.canRead() && output.canWrite())
-                makeQuiz(input, output);
+                for(int i = 0; i < 20; i++)
+                    makeQuiz(input, output);
         } catch(FileNotFoundException ioe) {
             System.out.println("File not found");
             ioe.printStackTrace();
@@ -30,9 +33,10 @@ public class QuizMaker {
     }
     
     public static void makeQuiz(File in, File out) {
-        HashMap<Key, String> map = loadStateHash(in);
-        List<Key> keys = new ArrayList<>(map.keySet());
+        HashMap<String, String> map = loadStateHash(in);
+        List<String> keys = new ArrayList<>(map.keySet());
         ArrayList<String> quizQuestions = new ArrayList<>();
+        ArrayList<String> usedStates = new ArrayList<>();
         for(int i = 0; i < 50; i++) {
             String question = (i+1)+". The capital of ";
             String state = "";
@@ -40,18 +44,19 @@ public class QuizMaker {
             boolean valid = false;
             while(!valid) {
                 int rand = random(0,keys.size());
-                if(!keys.get(rand).used) {
-                    state = keys.get(rand).str;
+                if(!usedStates.contains(keys.get(rand))) {
+                    state = keys.get(rand);
                     valid = true;
                     question += state+" is...\n";
+                    usedStates.add(keys.get(rand));
                 }
             }
             ArrayList<String> answers = new ArrayList<>(4);
             System.out.println(answers);
-            answers.add(map.get(new Key(state)));//set the real answer to a random one from 1 to 4
+            answers.add(map.get(new String(state)));//set the real answer to a random one from 1 to 4
             System.out.println(answers);
-            while(answers.size() < 4)
-                if(!keys.get(random(0,keys.size())).str.equals(state))
+            for(int j = 0; j < 3; j++)
+                if(!keys.get(random(0,keys.size())).equals(state))
                     answers.add(map.get(keys.get(random(0,keys.size()))));
             System.out.println(answers);
             //Collections.shuffle(answers);
@@ -68,9 +73,9 @@ public class QuizMaker {
         if(list.isEmpty()) {
             System.out.println("states.txt is either empty or nonexistent");
         }
-        HashMap<Key, String> map = new HashMap<Key, String>();
+        HashMap<String, String> map = new HashMap<String, String>();
         while(!list.isEmpty())
-            map.put(new Key(list.remove(0)), list.remove(0));
+            map.put(new String(list.remove(0)), list.remove(0));
         return map;
     }
     
@@ -119,13 +124,5 @@ public class QuizMaker {
     
     private static void print(Object o) {
         System.out.println(o);
-    }
-    public static class Key {
-        public String str;
-        public boolean used;
-        public Key(String in) {
-            str = in;
-            used = false;
-        }
     }
 }
